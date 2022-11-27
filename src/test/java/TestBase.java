7f03f047860cf37196cb3b72163c770b850d6fa6
Sebastian Falana <s.falana@azagroup.eu>
2022-11-27 15:21:08 +0100

    Test base

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+
+    public String baseUrl = "https://jsonplaceholder.typicode.com/users/";
+
+    @BeforeMethod
+    public void  setup(){
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+}
