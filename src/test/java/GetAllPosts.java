b1b42dc119d63bb3212ffa06563d7408493dbde7
Sebastian Falana <s.falana@azagroup.eu>
2022-11-27 14:57:10 +0100

    My second test on Rest Assured

--- src/test/java/GetAllPosts.java
+++ src/test/java/GetAllPosts.java
@@ -1,5 +1,9 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 public class GetAllPosts {
@@ -20,5 +24,24 @@ public class GetAllPosts {
                 .statusCode(200)
                 .log()
                 .all();
+    }
+
+    @Test
+    public void  shouldGetFirstPostAndValidateJson() {
+        Response response =
+                given().
+                        log().
+                        all().
+        when()
+                .get("https://jsonplaceholder.typicode.com/users/1")
+                .then()
+                .statusCode(200)
+                .log()
+                .all()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Assert.assertEquals(jsonPath.get("address.geo.lat"), "-37.3159");
     }
 }
