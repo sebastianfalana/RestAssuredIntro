31c37ec18d4d63fbf70374b730bf6af59a24c700
Sebastian Falana <s.falana@azagroup.eu>
2022-12-07 10:41:55 +0100

    Homework ResAssure

---
+++ src/test/java/GetAllTodos.java
@@ -0,0 +1,48 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class GetAllTodos extends TestBase{
+
+    String todos = "/todos";
+    @Test
+    public void shouldGetAllTodos(){
+        when()
+                .get(baseUrl+todos)
+                .then()
+                .statusCode(200);
+
+    }
+
+    @Test
+    public void shouldGetFourthTodos(){
+        Response response =
+        when()
+                .get(baseUrl+todos+"/4")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Assert.assertEquals(jsonPath.get("id"), "1");
+
+    }
+
+    @Test
+    public void shouldGetAllTodosWithUserId4(){
+        given()
+                .queryParam("userId","1").
+                when()
+                .get(baseUrl+todos).
+                then()
+                .statusCode(200);
+
+    }
+
+
+}
